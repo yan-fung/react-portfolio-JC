0e8014d7f77ad8322cdfba6263179499bea7b1b6
Yan Fung <fungmingyan@hotmail.com>
2023-10-07 14:20:08 +0100

    Rendering the Navbar and Home components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import Home from "./components/Home";
+import Sidenav from "./components/Sidenav";
+
 const App = () => {
-  return <div>Hello World!</div>;
+  const [selectedPage, setSelectedPage] = useState("home");
+  return (
+    <div>
+      <Sidenav selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div className='mx-auto md:h-full'>
+        <Home setSelectedPage={setSelectedPage} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
