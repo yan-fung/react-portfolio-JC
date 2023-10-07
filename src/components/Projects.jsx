3c99539e2e403287558ce805012c5dfc4c1ac706
Yan Fung <fungmingyan@hotmail.com>
2023-10-07 17:24:47 +0100

    Established an initial draft for the project section; content and additional images to be refined

---
+++ src/components/Projects.jsx
@@ -0,0 +1,53 @@
+const Projects = () => {
+  return (
+    <section id='projects' className='md:pr-60 md:pl-60 pr-3 pl-3'>
+      <div className='text-center'>
+        <h1 className='font-playfair text-4xl pb-10 pt-40'>Projects</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
+          necessitatibus exercitationem eaque odit? Laboriosam cupiditate
+          necessitatibus dolores accusamus. Labore quibusdam tempore ipsum
+          numquam veniam voluptas deleniti fugit consequatur? Consequuntur,
+          aliquam.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
+          necessitatibus exercitationem eaque odit? Laboriosam cupiditate
+          necessitatibus dolores accusamus. Labore quibusdam tempore ipsum
+          numquam veniam voluptas deleniti fugit consequatur? Consequuntur,
+          aliquam.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
+          necessitatibus exercitationem eaque odit? Laboriosam cupiditate
+          necessitatibus dolores accusamus. Labore quibusdam tempore ipsum
+          numquam veniam voluptas deleniti fugit consequatur? Consequuntur,
+          aliquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Quas necessitatibus exercitationem eaque odit? Laboriosam cupiditate
+          necessitatibus dolores accusamus. Labore quibusdam tempore ipsum
+          numquam veniam voluptas deleniti fugit consequatur? Consequuntur,
+          aliquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Quas necessitatibus exercitationem eaque odit? Laboriosam cupiditate
+          necessitatibus dolores accusamus. Labore quibusdam tempore ipsum
+          numquam veniam voluptas deleniti fugit consequatur? Consequuntur,
+          aliquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Quas necessitatibus exercitationem eaque odit? Laboriosam cupiditate
+          necessitatibus dolores accusamus. Labore quibusdam tempore ipsum
+          numquam veniam voluptas deleniti fugit consequatur? Consequuntur,
+          aliquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Quas necessitatibus exercitationem eaque odit? Laboriosam cupiditate
+          necessitatibus dolores accusamus. Labore quibusdam tempore ipsum
+          numquam veniam voluptas deleniti fugit consequatur? Consequuntur,
+          aliquam. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Quas necessitatibus exercitationem eaque odit? Laboriosam cupiditate
+          necessitatibus dolores accusamus. Labore quibusdam tempore ipsum
+          numquam veniam voluptas deleniti fugit consequatur? Consequuntur,
+          aliquam.
+        </p>
+        <img src='https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80' />
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
