5cbc0c92ce09b0593a07895dbeea331091cfb856
Yan Fung <fungmingyan@hotmail.com>
2023-10-08 23:11:02 +0100

    Implemented downloadable CV link, enhance personal summary and refine styling for an improved user experience

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,7 +25,7 @@ const Home = ({ setSelectedPage }) => {
         {isAboveMediumScreens && (
           <div className=''>
             <img
-              className='hover:filter hover:saturate-50 transition duration-500 z-10 w-full max-w-[250px] md:max-w-[450px]'
+              className='hover:filter hover:saturate-50 transition duration-500 z-10 w-full md:max-w-[450px]'
               alt='profile-pic'
               src={profilePic}
             />
@@ -43,24 +43,31 @@ const Home = ({ setSelectedPage }) => {
           }}
         >
           <div
-            className={` font-playfair text-center absolute md:top-10 w-full ${
-              isAboveMediumScreens ? "left-[30%]" : "left-0 h-full"
+            className={`font-playfair text-left absolute md:top-8 w-full ${
+              isAboveMediumScreens ? "left-[28%]" : "left-0 h-full"
             } w-full md:w-[40%]`}
           >
-            <h1 className='text-4xl pt-10 pb-5'>Theatre Professional</h1>
-            <p className='text-lg md:text-2xl pr-3 pl-3 pb-5'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
-              pariatur a reiciendis saepe qui dignissimos aliquam facere
-              officiis iusto minus nam velit ratione fugiat natus harum itaque
-              inventore, sequi neque?
-            </p>
-
-            <p className='text-lg md:text-2xl pr-3 pl-3 pb-5'>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
-              pariatur a reiciendis saepe qui dignissimos aliquam facere
-              officiis iusto minus nam velit ratione fugiat natus harum itaque
-              inventore, sequi neque?
-            </p>
+            <h1 className='text-center text-2xl md:text-4xl pt-10 md:pt-5'>
+              THEATRE PROFESSIONAL
+            </h1>
+            <h2 className='text-center text-2xl md:text-4xl pt-5 pb-5'>
+              Writer | Director | Performer
+            </h2>
+            <div>
+              <p className='text-base md:text-2xl pr-3 pl-3 pb-5'>
+                Possessing extensive experience as a theater director, my
+                proficiency encompasses the domains of writing, directing, and
+                performance within the United Kingdom.
+              </p>
+            </div>
+            <div>
+              <p className='text-base md:text-2xl pr-3 pl-3 pb-5'>
+                Distinguished achievements involve orchestrating theatrical
+                productions on a national scale, with a notable pinnacle being
+                the adept direction of a touring comedy show culminating at the
+                prestigious Edinburgh Fringe Festival in 2019.
+              </p>
+            </div>
             <motion.div
               className='flex mt-10 justify-center text-lg md:text-2xl'
               initial='hidden'
@@ -72,21 +79,23 @@ const Home = ({ setSelectedPage }) => {
                 visible: { opacity: 1, x: 0 },
               }}
             >
-              <AnchorLink
-                className=''
-                onClick={() => setSelectedPage("contact")}
-                href='#contact'
-              >
-                <div className='font-semibold hover:text-white transition duration-500 w-full h-full flex items-cent-dark-piner justify-center px-8 border border-yellow'>
-                  Contact Me
+              <div>
+                <div className='font-semibold hover:text-white transition duration-500 w-58  h-full flex items-cent-dark-piner justify-center px-2 border border-yellow'>
+                  <a
+                    target='_blank'
+                    href='https://media.licdn.com/dms/document/media/C562DAQGmOHyCuvSq6A/profile-treasury-document-pdf-analyzed/0/1643715605734?e=1697673600&v=beta&t=ewcQeHIL-BMDRMnthOd_eWgUaSyAwQq2paV2kaFQDOI'
+                    rel='noreferrer'
+                  >
+                    Download my CV
+                  </a>
                 </div>
-              </AnchorLink>
+              </div>
               <AnchorLink
                 className='rounded-r-sm bg-yellow py-0.5 pr-0.5'
                 onClick={() => setSelectedPage("contact")}
                 href='#contact'
               >
-                <div className='font-semibold hover:text-white transition duration-500 w-full h-full flex items-center justify-center px-10 text-black'>
+                <div className='font-semibold hover:text-white transition duration-500 w-58 h-full flex items-center justify-center px-12 text-black'>
                   Let&apos;s talk
                 </div>
               </AnchorLink>
